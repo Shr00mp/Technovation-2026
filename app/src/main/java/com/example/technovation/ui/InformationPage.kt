6d64f1e3a7e268c9c332459e7e7474f9bc2a3964
kitkat-9 <k.tyagi@spgs.org>
2026-04-05 21:10:10 +0100

    Created database for resources, finished queries, added type converters

--- app/src/main/java/com/example/technovation/ui/InformationPage.kt
+++ app/src/main/java/com/example/technovation/ui/InformationPage.kt
@@ -1,9 +1,16 @@
 package com.example.technovation.ui
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.room3.Dao
+import androidx.room3.Database
 import androidx.room3.Entity
 import androidx.room3.PrimaryKey
 import androidx.room3.Query
+import androidx.room3.Room
+import androidx.room3.RoomDatabase
+import androidx.room3.TypeConverter
+import androidx.room3.TypeConverters
 import kotlinx.coroutines.flow.Flow
 
 enum class Type {ARTICLE, VIDEO}
@@ -51,4 +58,48 @@ interface ContentDao {
         typeFilter: String,
         categoryFilter: String
     ): Flow<List<Content>>
+
+   @Query("SELECT * FROM content WHERE category = :category")
+   fun filterByCategory(category: Category): Flow<List<Content>>
+
+   @Query("SELECT * FROM content WHERE type = :type")
+   fun filterByType(type: Type): Flow<List<Content>>
+
+   @Query("UPDATE content SET saved = :saved WHERE id = :id")
+   suspend fun updateSaved(id: Int, saved: Boolean)
+}
+
+//These are needed to convert from the self-defined enum classes to actual data types
+class Converters {
+    @TypeConverter
+    fun fromType(value: Type): String = value.name
+
+    @TypeConverter
+    fun toType(value: String): Type = Type.valueOf(value)
+
+    @TypeConverter
+    fun fromCategory(value: Category): String = value.name
+
+    @TypeConverter
+    fun toCategory(value: String): Category = Category.valueOf(value)
+}
+
+@SuppressLint("RestrictedApi")
+@Database(entities = [Content::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class ResourcesDatabase : RoomDatabase(){
+    abstract fun contentDao(): ContentDao
+    @Volatile private var INSTANCE: ResourcesDatabase? = null
+
+    //this means the database is only created once avoiding conflicts
+    fun getDatabase(context: Context): ResourcesDatabase {
+        return INSTANCE ?: synchronized(this) {
+            Room.databaseBuilder(
+                context.applicationContext,
+                ResourcesDatabase::class.java,
+                "health_content_db"
+            ).build().also { INSTANCE = it }
+        }
+    }
+
 }
